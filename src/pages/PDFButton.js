3b867b9e014fb0542061eebba6c418522753aec7
Bazzer588 <40038437+Bazzer588@users.noreply.github.com>
2019-06-15 23:03:23 +0100

    pdf button

--- src/pages/PDFButton.js
+++ src/pages/PDFButton.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Button from "../components/Button";
 
-export default function PDFButton ({ parent }) {
+export default function PDFButton({parent}) {
 
-    const { pdfReady } = (parent.state || {});
+    const {pdfReady} = (parent.state || {});
 
     return (
         <>
             <Button onClick={() => getPDF(parent)}>Get PDF</Button>
             {(pdfReady || window.PDF_BLOB) &&
+            <>
                 <Button onClick={() => downloadPDF()}>Download</Button>
+                {' '}
+                <a href={window.PDF_URL} target="_blank">Open PDF</a>
+            </>
             }
         </>
     );
@@ -23,16 +27,18 @@ const getPDF = (parent) => {
             debugHeaders(response.headers);
             return response.blob();
         })
-        .then(blob=> {
-            console.log('BLOB',blob.size);
-            parent.setState({ pdfReady: true });
+        .then(blob => {
+            console.log('BLOB', blob.size);
             window.PDF_BLOB = blob;
+            window.PDF_URL = window.URL.createObjectURL(blob);
+            console.log('PDF_URL',window.PDF_URL);
+            parent.setState({pdfReady: true});
         });
 };
 
-function debugHeaders (h) {
-    h.forEach( (val,name) => {
-        console.log(name,':',val);
+function debugHeaders(h) {
+    h.forEach((val, name) => {
+        console.log(name, ':', val);
     });
 }
 
@@ -41,15 +47,23 @@ const downloadPDF = () => {
     // var newBlob = new Blob([blob], {type: "application/pdf"}) // is this needed ?
 
     if (window.navigator.msSaveOrOpenBlob) {
-        window.navigator.msSaveOrOpenBlob(blob,'downloaded.pdf');
+        window.navigator.msSaveOrOpenBlob(blob, 'downloaded.pdf');
     } else {
         const link = window.document.createElement('a');
         link.href = window.URL.createObjectURL(blob);
+        console.log('OBJECT URL', link.href);
         link.download = 'policy docs.pdf';
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
         // clean up
-        setTimeout(() => window.URL.revokeObjectURL(link.href), 100 );;
+        // setTimeout(() => window.URL.revokeObjectURL(link.href), 100 );;
     }
 };
+
+/*
+    This is the blob link, not pretty:
+
+    blob:http://localhost/4e89d69a-ac87-4473-9e18-f837a45cf9f7
+
+ */
